a35a8ed7ba4f9171b8ab8d415ee24a220bdef66c
Sehne, Julian <julian.sehne@student.fhws.de>
2023-05-27 18:22:13 +0200

    Logs not showing

--- app/src/main/java/com/example/velocity/MainActivity.kt
+++ app/src/main/java/com/example/velocity/MainActivity.kt
@@ -1,9 +1,8 @@
-package com.example.velocity
-
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,8 +13,7 @@ import com.example.velocity.R
 class MainActivity : AppCompatActivity() {
 
     private lateinit var locationManager: LocationManager
-    private var previousLocation: Location? = null
-    private var previousTimestamp: Long = 0
+    private var locationListener: LocationListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED
         ) {
+            Log.i("MainActivity_debug", "ActivityCompat.checkSelfPermission")
             // Request location permission
             ActivityCompat.requestPermissions(
                 this,
@@ -36,34 +35,52 @@ class MainActivity : AppCompatActivity() {
             )
         } else {
             // Location permission already granted
-            getLocation()
+            Log.i("MainActivity_debug", "startLocationUpdates")
+            startLocationUpdates()
         }
     }
 
-    private fun getLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED
+    private fun startLocationUpdates() {
+        Log.i("MainActivity_debug", "startLocationUpdates")
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                // Do something with the updated location
+                val velocity = location.speed
+
+                Log.i("MainActivity_debug", "$velocity")
+            }
+
+            override fun onProviderDisabled(provider: String) {}
+
+            override fun onProviderEnabled(provider: String) {}
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+        }
+
+        // Register the location listener to receive periodic updates
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // Get the last known location from the provider
-            val location: Location? =
-                locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-
-        // Check if there is a previous location
-        if (previousLocation != null && location != null) {
-            val currentTimestamp = System.currentTimeMillis()
-
-            // Calculate the time difference in seconds
-            val timeDifference = (currentTimestamp - previousTimestamp) / 1000.0
-
-            // Calculate the distance between the previous and current location
-            val distance = previousLocation!!.distanceTo(location)
-
-            // Calculate the velocity (speed) in meters per second
-            val velocity = distance / timeDifference
-
-            Log.i("MainActivity", "Velocity: $velocity m/s")
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
         }
-        }
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            LOCATION_UPDATE_INTERVAL,
+            MIN_DISTANCE_CHANGE_FOR_UPDATES,
+            locationListener as LocationListener
+        )
     }
 
     override fun onRequestPermissionsResult(
@@ -75,16 +92,24 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == REQUEST_LOCATION_PERMISSION) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Location permission granted
-                getLocation()
+                startLocationUpdates()
             } else {
-                print("ACCESS DENIED")
+                Log.i("MainActivity_debug", "ACCESS DENIED")
                 // Location permission denied
                 // Handle the denial or disable location-related functionality
             }
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        // Unregister the location listener when the activity is destroyed
+        locationManager.removeUpdates(locationListener as LocationListener)
+    }
+
     companion object {
         private const val REQUEST_LOCATION_PERMISSION = 123
+        private const val LOCATION_UPDATE_INTERVAL: Long = 1000 // 1 second
+        private const val MIN_DISTANCE_CHANGE_FOR_UPDATES: Float = 0f // 0 meters
     }
 }
